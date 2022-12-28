0864a841d7a342a5957f8d236da7a34c40aa9e5c
xiao1422 <81155010+xiao1422@users.noreply.github.com>
2022-12-28 16:30:03 +0800

    关闭组件命名规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,13 +17,13 @@ module.exports = {
         'space-before-function-paren': 0,
         'no-tabs': 'off',
         // 关闭组件命名规则
-        // 'vue/multi-word-component-names': 'off'
+        'vue/multi-word-component-names': 'off'
         // 添加组件命名忽略规则
-        'vue/multi-word-component-names': [
-            'error',
-            {
-                ignores: ['index']
-            }
-        ]
+        // 'vue/multi-word-component-names': [
+        //     'error',
+        //     {
+        //         ignores: ['index']
+        //     }
+        // ]
     }
 }
