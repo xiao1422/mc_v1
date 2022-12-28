733ee13479c2c244159a4d30b87b19db79cf834c
xiao1422 <81155010+xiao1422@users.noreply.github.com>
2022-12-28 14:27:23 +0800

    layout布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/login_v1/login_v1')
-  }
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/login')
+    },
+    {
+        path: '/',
+        name: '/',
+        component: () => import('../layout')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
